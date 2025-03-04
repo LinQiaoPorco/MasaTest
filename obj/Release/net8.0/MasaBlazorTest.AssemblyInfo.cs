d66fbd0ec592afdd73bb375198df36b9201ae34b
LinQiaoPorco <qiao.lin@volvocars.com>
2025-03-04 16:56:17 +0800

    Restore

--- obj/Release/net8.0/MasaBlazorTest.AssemblyInfo.cs
+++ obj/Release/net8.0/MasaBlazorTest.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("MasaBlazorTest")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0d4fa30ce5a2147a15bc846d8331f8871d4995c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MasaBlazorTest")]
